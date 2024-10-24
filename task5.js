427843b79f3551e883c38a382e838e3b6fe887ec
vivek patidar <70385245+06vivek@users.noreply.github.com>
2024-10-24 13:59:02 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,9 @@
+// 1. Take 2 numbers and print the smallest one amongst them
+// 2. Take 3 numbers and print the biggest one amongst them
+// 3. Use switch case to return a grade for the score, either "A", "B", "C", "D", or "F". (Take convension for numbers and grades by yourself)
+// Anything below 50 is F
+let num1 =10;
+let num2 =12;
+let smallestNum = (num1<num2)? `num1 is smallest `:`num2 is smallest`;
+
+console.log(smallestNum)
